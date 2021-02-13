c0e0346c5f67658309e933899bf27e408d881a78
Sanmuell <sanmuell.i@gmail.com>
2021-02-13 20:10:17 -0300

    Projeto Criado

---
+++ src/main/java/com/spring/ionic/CursoSpringIonicApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringIonicApplication.class, args);
+	}
+
+}
